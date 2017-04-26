107b222de9d633ab584efa7815623785ddc4d6f3
gilmae <avocadia@gmail.com>
2017-04-26 19:29:11 +1000

    Add flags

--- gradient_gen.go
+++ gradient_gen.go
@@ -13,52 +13,32 @@ import (
   "sort"
   "encoding/hex"
   "encoding/json"
+  "flag"
 )
 
-func main() {
-
-  var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
-
-  var points = uint8(rng.Float64() * 4.0) + 3
-  var xSequence = make([]float64, points)
-  var redpoints = make([]float64, points)
-  var greenpoints = make([]float64, points)
-  var bluepoints = make([]float64, points)
-
-  for i,_:= range xSequence {
-    xSequence[i] = rng.Float64()
-    redpoints[i] = rng.Float64()*255.0
-    greenpoints[i] = rng.Float64()*255.0
-    bluepoints[i] = rng.Float64()*255.0
-  }
-
-  sort.Float64s(xSequence)
-
-  var redInterpolant interpolation.MonotonicCubic = interpolation.CreateMonotonicCubic(xSequence, redpoints)
-  var greenInterpolant interpolation.MonotonicCubic = interpolation.CreateMonotonicCubic(xSequence, greenpoints)
-  var blueInterpolant interpolation.MonotonicCubic = interpolation.CreateMonotonicCubic(xSequence, bluepoints)
-
+func generate_swatch(filename string, drawLine bool, redInterpolant interpolation.MonotonicCubic, greenInterpolant interpolation.MonotonicCubic, blueInterpolant interpolation.MonotonicCubic) {
   bounds := image.Rect(0,0,300,50)
   b := image.NewNRGBA(bounds)
   draw.Draw(b, bounds, image.NewUniform(color.Black), image.ZP, draw.Src)
 
-  for x:=0 ; x < 300 ; x++ {
-    for y:= 0; y < 50 ; y++ {
-      var point = float64(x) / 300.0
-      var redpoint = redInterpolant(point)
-      var greenpoint = greenInterpolant(point)
-      var bluepoint = blueInterpolant(point)
+  line_colour := color.NRGBA{255,255,255, 255}
 
+  for x:=0 ; x < 640 ; x++ {
+    var point = float64(x) / 300.0
+    var redpoint = redInterpolant(point)
+    var greenpoint = greenInterpolant(point)
+    var bluepoint = blueInterpolant(point)
+
+    for y:= 0; y < 255 ; y++ {
       c := color.NRGBA{uint8(redpoint), uint8(greenpoint), uint8(bluepoint), 255}
-
       b.Set(x,y,c)
     }
-  }
 
-  var filename string = "test.jpg"
-
-  if (len(os.Args) > 1) {
-    filename = os.Args[1]
+    if (drawLine) {
+      b.Set(x, 255-int(redpoint), line_colour)
+      b.Set(x, 255-int(greenpoint), line_colour)
+      b.Set(x, 255-int(bluepoint), line_colour)
+    }
   }
 
   file, err := os.Create(filename)
@@ -73,6 +53,43 @@ func main() {
   if err = file.Close();err != nil {
     fmt.Println(err)
   }
+}
+
+func main() {
+
+  var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+  var filename string
+  var showRgbLines bool
+
+  flag.StringVar(&filename, "f", "", "Generate a swatch to the input filepath.")
+  flag.BoolVar(&showRgbLines, "l", false, "Show the RGB curve in the swatch.")
+
+  flag.Parse()
+
+  var points = uint8(rng.Float64() * 3.0) + 4
+  var xSequence = make([]float64, points)
+  var redpoints = make([]float64, points)
+  var greenpoints = make([]float64, points)
+  var bluepoints = make([]float64, points)
+
+  for i,_:= range xSequence {
+    xSequence[i] = rng.Float64()
+    redpoints[i] = rng.Float64()*255.0
+    greenpoints[i] = rng.Float64()*255.0
+    bluepoints[i] = rng.Float64()*255.0
+  }
+
+xSequence[0] = 0.0
+xSequence[1] = 1.0
+  sort.Float64s(xSequence)
+
+  var redInterpolant interpolation.MonotonicCubic = interpolation.CreateMonotonicCubic(xSequence, redpoints)
+  var greenInterpolant interpolation.MonotonicCubic = interpolation.CreateMonotonicCubic(xSequence, greenpoints)
+  var blueInterpolant interpolation.MonotonicCubic = interpolation.CreateMonotonicCubic(xSequence, bluepoints)
+
+  if (filename != "") {
+    generate_swatch(filename, showRgbLines, redInterpolant, greenInterpolant, blueInterpolant)
+  }
 
   var gradient  [][]string
 
@@ -83,7 +100,7 @@ func main() {
     gradient[i] = []string {fmt.Sprintf("%.6f", xSequence[i]), hexRGB}
   }
 
-  j, err := json.Marshal(gradient)
+  j, _ := json.Marshal(gradient)
   var jsonString = string(j[:])
 
   fmt.Println(jsonString)
